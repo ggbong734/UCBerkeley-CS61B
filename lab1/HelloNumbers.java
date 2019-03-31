f95c79a5f0a7561f7bf452b8383cabd1c50cf148
Gerry Bong <ggbong@Gerry-Samsung-Laptop.local>
2019-03-30 18:37:40 -0700

    completed first part of lab 1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,17 @@
+public class ClassNameHere {
+   public static int max(int[] m) {
+      int c = 0;
+      int high = -999;
+         while (c < m.length) {
+            if (m[c] > high) {
+                high = m[c];
+            }
+            c = c + 1;
+      }
+      return high;
+   }
+   public static void main(String[] args) {
+      int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6}; 
+      System.out.println(max(numbers));
+   }
+}
