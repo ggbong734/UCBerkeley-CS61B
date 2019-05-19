a3d56c97a0ee9a2f328109643d6df4699e826634
Gerry Bong <ggbong@Gerry-Samsung-Laptop.local>
2019-05-19 16:01:10 -0700

    completed hw4

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -1,33 +1,28 @@
 package bearmaps.hw4;
 
-import bearmaps.proj2ab.DoubleMapPQ;
+import bearmaps.proj2ab.ArrayHeapMinPQ;
+import bearmaps.proj2ab.ExtrinsicMinPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /** @author Gerry Bong
  *  this is hw4 of CS61B Sp 2019 course
- *  for with pseudocode help from Josh Hug
+ *  for with pseudocode help from Josh Hug and yngz
+ *  https://github.com/yngz/cs61b/blob/master/hw4/bearmaps/hw4/AStarSolver.java
  *
  * @param <Vertex>
  */
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
 
-    private DoubleMapPQ<Vertex> fringe;
     private SolverOutcome outcome;
-    private ArrayList<Vertex> solutionArray;
+    private LinkedList<Vertex> solutionArray;
     private double solutionWeight;
     private int numStatesExplored;
     private double explorationTime;
-
-    private HashMap<Vertex, Double> distTo;
-    private HashMap<Vertex, Vertex> edgeTo;
-    private HashSet<Vertex> visitedVertices;
-
 
     /** Constructor which finds the solution, computing everything necessary for
      * all other methods to return their results in constant time.
@@ -37,15 +32,15 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                        Vertex end, double timeout) {
         Stopwatch sw = new Stopwatch();
 
-        fringe = new DoubleMapPQ<>();
-        solutionArray = new ArrayList<>();
         outcome = null;
+        solutionArray = new LinkedList<>();
         solutionWeight = 0;
         numStatesExplored = 0;
         explorationTime = 0;
-        distTo = new HashMap<>();
-        edgeTo = new HashMap<>();
-        visitedVertices = new HashSet<>();
+
+        ExtrinsicMinPQ<Vertex> fringe = new ArrayHeapMinPQ<>();
+        HashMap<Vertex, Double> distTo = new HashMap<>();
+        HashMap<Vertex, Vertex> edgeTo = new HashMap<>();
 
         // Checks if start condition is end
         if (start.equals(end)) {
@@ -55,78 +50,59 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         }
 
         // Insert source vertex into PQ and insert source into
-        fringe.add(start, 0);
         distTo.put(start, 0.0);
+        fringe.add(start, distTo.get(start)
+                + input.estimatedDistanceToGoal(start, end));
 
         //exit loop if PQ is empty or end vertex is the first of PQ
-        while (fringe.size() != 0 && !fringe.getSmallest().equals(end)) {
+        while (fringe.size() > 0 && !fringe.getSmallest().equals(end)
+                && sw.elapsedTime() < timeout) {
 
             Vertex p = fringe.removeSmallest();
-            visitedVertices.add(p);
             numStatesExplored += 1;
-            List<WeightedEdge<Vertex>> neighborEdges = input.neighbors(p);
 
             //relax only outgoing edges
+            List<WeightedEdge<Vertex>> neighborEdges = input.neighbors(p);
             for (WeightedEdge<Vertex> edge : neighborEdges) {
 
-                Vertex s = edge.from();    // same as p (can remove)
                 Vertex t = edge.to();
                 double edgeWeight = edge.weight();
 
                 // relaxation fails if target vertex is already visited
                 // add new distance if vertex is not relaxed
                 // otherwise if new edge is better, update distance and edge
-                if (visitedVertices.contains(t)) {
-                    continue;   //continue to next iteration instead of breaking loop
-                } else if ((!distTo.containsKey(t))) {
-                    distTo.put(t, distTo.get(s) + edgeWeight);
-                    edgeTo.put(t, s);
-                } else if (distTo.get(s) + edgeWeight < distTo.get(t)) {
-                    distTo.replace(t, distTo.get(s) + edgeWeight);
-                    edgeTo.replace(t, s);
-                }
+                if (!distTo.containsKey(t) || distTo.get(p) + edgeWeight < distTo.get(t)) {
 
-                // add relaxed vertex to fringe if not already present.
-                // if vertex is already in fringe, update priority.
-                double newPriority = distTo.get(t) + input.estimatedDistanceToGoal(t, end);
-                if (!fringe.contains(t)) {
-                    fringe.add(t, newPriority);
-                } else {
-                    fringe.changePriority(t, newPriority);
+                    distTo.put(t, distTo.get(p) + edgeWeight);
+                    edgeTo.put(t, p);
+                    // add relaxed vertex to fringe if not already present.
+                    // if vertex is already in fringe, update priority.
+                    double newPriority = distTo.get(t) + input.estimatedDistanceToGoal(t, end);
+                    if (!fringe.contains(t)) {
+                        fringe.add(t, newPriority);
+                    } else {
+                        fringe.changePriority(t, newPriority);
+                    }
                 }
             }
 
-            explorationTime = sw.elapsedTime();
-
-            //exit if goal vertex is the first in PQ
-            if (fringe.getSmallest().equals(end)) {
+            //exit if fringe PQ is empty or goal vertex is the first in PQ
+            //or if timeout value is exceeded.
+            if (fringe.size() == 0) {
+                outcome = SolverOutcome.UNSOLVABLE;
+            } else if (fringe.getSmallest().equals(end)) {
                 outcome = SolverOutcome.SOLVED;
                 // to get the solution array and weight
-                buildSolutionArray(end, solutionArray);
                 solutionWeight = distTo.get(end);
-                break;
-            }
-
-            //exit if time spent exceeds timeout value
-            if (explorationTime > timeout) {
+                Vertex n = fringe.getSmallest();
+                while (n != null) {
+                    solutionArray.addFirst(n);
+                    n = edgeTo.get(n);
+                }
+            } else {
                 outcome = SolverOutcome.TIMEOUT;
-                break;
             }
-
-            //otherwise this graph is unsolvable
-            outcome = SolverOutcome.UNSOLVABLE;
-        }
-    }
-
-    // recursively trace the shortest path (in edgeTo array) from the end goal
-    // to add the shortest path vertices to the solution array.
-    private void buildSolutionArray(Vertex t, ArrayList<Vertex> solArray) {
-        if (!edgeTo.containsKey(t)) {
-            solArray.add(t);
-        } else {
-            Vertex s = edgeTo.get(t);
-            buildSolutionArray(s, solArray);
-            solArray.add(t);
+            explorationTime = sw.elapsedTime();
         }
     }
 
